15ce9dea1668a9d08957885f72999634045415f7
Andrew Choi <choiandrew585@gmail.com>
2024-07-21 14:07:56 -0400

    Add template HTML file for later implentation

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var http_1 = require("http");
+var PORT = 8000;
+var server = http_1.default.createServer(function (req, res) {
+});
+server.listen(PORT, '127.0.0.1');
